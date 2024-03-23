c84399466086fe6dd62e142be395892c33bf8311
Seb Karena <EKChikui@gmail.com>
2024-03-23 12:10:13 -0300

    Create autobackup.py

---
+++ AutoGitHub/autobackup.py
@@ -0,0 +1,25 @@
+# AutoBackup [P164]
+# Automatic backup for Book2 and external driver
+
+
+# Libraries
+import os
+import sys
+
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+
+
+
+
+# end
+
