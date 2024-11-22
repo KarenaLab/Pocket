8ed58a103f5392d8eeb9dddedb238a48b794c0b4
Seb Karena <EKChikui@gmail.com>
2024-11-21 22:17:15 -0300

    Update b3_invest_control.py

--- B3 Invest/b3_invest_control.py
+++ B3 Invest/b3_invest_control.py
@@ -13,7 +13,12 @@ import matplotlib.pyplot as plt
 
 # Personal modules
 sys.path.append(r"c:\python_modules")
-from b3_invest_tools import read_portfolio
+from b3_invest_tools import (_reset_portfolio,
+                             _export_portfolio,
+                             read_portfolio,
+                             add_operation,
+                             view_portfolio,
+                             update_database)
 
 
 # Functions
@@ -27,6 +32,39 @@ from b3_invest_tools import read_portfolio
 # Program --------------------------------------------------------------
 data = read_portfolio()
 
+while(True):
+    print(" > [a]dd operation,")
+    print(" > [q]uit and save portfolio")
+    print(" > [f]inish without save portfolio")
+    print(" > [v]iew portfolio")
+    print("")
+    print(" > [r]eset portfolio")
+    
+    print("")
+    
+    decision = input(" > Choose your action: ")
+
+
+    if(decision == "a"):
+        data = add_operation(data)
+
+
+    if(decision == "r"):
+        data = _reset_portfolio()
+        _export_portfolio(data)
+
+
+    if(decision == "v"):
+        view_portfolio(data)
+
+
+    if(decision == "q"):
+        _export_portfolio(data)
+        break
+
+
+    if(decision == "f"):
+        break
 
 
 # end
