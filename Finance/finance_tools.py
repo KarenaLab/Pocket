bb2e9c22581238adae5399c17c05f9cf07aa286e
Seb Karena <EKChikui@gmail.com>
2025-01-18 08:29:51 -0300

    Update finance_tools.py

--- Finance/finance_tools.py
+++ Finance/finance_tools.py
@@ -25,7 +25,7 @@ def rate_annual_to_month(annual):
 def rate_month_to_annual(month):
     """
     Converts monthly investment rate into annual investment rate.
-    Equation: (1+a)^1 = (1+m)^12
+    Equation: (1+m)^12 = (1+a)^1
 
     """
     month = month / 100
@@ -35,3 +35,21 @@ def rate_month_to_annual(month):
     annual = np.round(annual, decimals=5)
 
     return annual
+
+
+def time_to_double(annual):
+    """
+    Calculates the time (months) to twice the interrest invested.
+    Equation: x = log(2) / log(1+i)
+
+    Return the number of months to twice the value.
+
+    """
+    monthly = rate_annual_to_month(annual)
+    monthly = monthly / 100
+
+    delta = np.log(2) / np.log(1+monthly)
+    
+
+    return delta
+
